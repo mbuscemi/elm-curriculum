38627a96b8e5eef46d3fdaa806e70f24ce89587b
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-10-21 07:55:13 -0700

    added lesson 1 / exercise 2

---
+++ lesson_01/exercise_02/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+model =
+    { result =
+        { id = 1
+        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+        , stars = 66
+        }
+    }
+
+
+elmHubHeader =
+    header []
+        [ h1 [] [ text "ElmHub" ]
+        , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
+        ]
+
+
+main =
+    div [ class "content" ]
+        [ text "TODO put the contents of elmHubHeader here instead of this text!"
+        , ul [ class "results" ]
+            [ li []
+                [ span [ class "star-count" ]
+                    [-- TODO display the number of stars here.
+                     --
+                     -- HINT: You'll need some parentheses to do this!
+                    ]
+
+                -- TODO use the model to put a link here that points to
+                -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
+                -- by prepending the "https://github.com/" part.
+                ]
+            ]
+        ]
